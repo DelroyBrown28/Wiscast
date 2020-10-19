e9266b2a62ee6c773f4d70a500553c6ebf466ac7
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-19 15:53:40 +0100

    can now get to purchase teir

---
+++ memberships/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import MembershipSelectView
+
+
+app_name = 'memberships'
+
+urlpatterns = [
+    path('', MembershipSelectView.as_view(), name='select'),
+
+]
+
--- wiscastMain/urls.py
+++ wiscastMain/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('podcasts.urls', namespace='podcasts')),
+    path('memberships/', include('memberships.urls', namespace='memberships')),
 ]
 
 if settings.DEBUG:
